5dc9b74aeb08f06c8daa2bfb92f70193cf269a94
PederHA <peder.andresen@gmail.com>
2021-02-12 08:31:00 +0100

    Add ngrok to install script

--- install_mcserver.sh
+++ install_mcserver.sh
@@ -2,15 +2,13 @@
 # Run this script in the directory where the various server directories should reside
 
 apt-get update && \
-apt-get install -y wget openjdk-11-jre-headless python3.8 python3.8-distutils unzip && \
-wget -O "get-pip.py" https://bootstrap.pypa.io/get-pip.py  && \
-python3.8 get-pip.py && \
-python3.8 -m pip install supervisor && \
-mkdir -p ~/.ngrok2 server logs && \
-cd server && \
+apt-get install -y wget openjdk-11-jre-headless unzip && \
+wget -O "ngrok.zip" https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && \
+unzip ngrok.zip && \
+rm ngrok.zip && \
+mkdir /root/.ngrok2 && \
+mv ngrok.yml /root/.ngrok2/ngrok.yml && \
+mv ngrok /root/.ngrok2/ngrok && \
+echo "authtoken: $NGROK_AUTHTOKEN" >> /root/.ngrok2/ngrok.yml && \
 wget -O "paper.jar" https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/457/downloads/paper-1.16.5-457.jar && \
 echo "eula=true" > eula.txt
-
-# cd server/spongeforge && \
-# wget -O spongeforge.jar https://repo.spongepowered.org/maven/org/spongepowered/spongeforge/1.12.2-2838-7.3.0/spongeforge-1.12.2-2838-7.3.0.jar
-# echo "eula=true" > eula.txt
